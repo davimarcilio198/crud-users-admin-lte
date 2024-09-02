1a9a2d28b5f1ede739c0976d2d6b23b27aa21a7c
Davi Marcilio <davi.burato@criciuma.unimedsc.com.br>
2024-09-02 14:55:47 -0300

    🔧Chore: Tailwindcss config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "tailwindcss";
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
+                "resources/sass/app.scss",
+                "resources/js/app.js",
+                "resources/css/app.css",
             ],
             refresh: true,
         }),
     ],
+    css: {
+        postcss: {
+            plugins: [tailwindcss()],
+        },
+    },
 });
